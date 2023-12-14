8af8b50775af466d744a19d654a0e9ef37780ff2
TomasPassadore <tomaspassadore@gmail.com>
2023-12-14 17:23:25 -0300

    Navbar settings

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 function Navbar() {
   return (
     <nav className="shadow-sm border-borderColor border fixed w-screen z-20 bg-backgroundColor">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <span className="self-center text-2xl border-b-2 py-1 border-[#4ac2c0] font-semibold whitespace-nowrap dark:text-white">Control Stock</span>
+      <div className="lg:ml-10 flex flex-wrap items-center justify-between mx-auto p-4">
+        <span className="self-center text-2xl border-b-2 py-1 border-[#4ac2c0] font-semibold whitespace-nowrap">Control Stock</span>
       </div>
     </nav>
   )
